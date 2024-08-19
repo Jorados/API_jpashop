61726faf101b52da479bf3179b5709edb5f9926b
seongjin8860 <seongjin8860@naver.com>
2024-08-19 22:17:43 +0900

    feat : ToMany 관계 fetch join 페이징 한계 돌파 -> ToOne 관계 먼저 fetch join과 페이징 후 hibernate.default_batch_fetch_size를 이용해서 추가 N 쿼리를 줄여라

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -7,22 +7,28 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
-
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -81,7 +87,62 @@ public class OrderApiController {
     }
 
 
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화 ---> 1+N+M 쿼리를 1+1+1 로 만들어준다.
+     */
 
+    // ToMany 관계 컬렉션과 페이징을 한번에 하려면 ??
+    // 1. ToOne 관계 모두 우선적으로 fetch join (계속 ToOne의 ToOne의 ToOne이라면 계속 fetch join 가능)
+    // 2. 컬렉션은 지연 로딩으로 조회한다.
+    // 3. 지연 로딩 성능최적화를 위해 hibernate.default_batch_fetch_size(글로벌), @BatchSize(개별) 를 적용한다.
+    // -> 이 설정은 기존에 따로따로 날아가야하는 1+N 쿼리에 대해서 IN 쿼리를 추가해서 설정 값 크기만큼 한번에 보내준다. 1+N에 대한 최적화가 어느정도 된거임 + 페이징도 가능해짐.
+    // -> v3 보다 DB에서 뽑아오는 데이터 전송량이 최적화됨.
+    // -> !! 컬렉션 fetch join은 페이징이 불가능하지만 이 방법은 페이징이 가능하다.
+    // -> 결론 : hibernate.default_batch_fetch_size 를 디폴트로 설정하자. (최소는 ( 보통 100개 )노상관 , 최대는 1000개 -> IN 쿼리 1000개 넘으면 오류뜨는 DB가 있음 )
+    //          이 값을 줄이면 총 시간이 증가하고, 이 값을 늘리면 DB에서 애플리케이션으로 데이터를 한번에 쫙보낼때 DB 부하가 생긴다. DB랑 WAS에서 부하가 견뎌지는 만큼 하면된다.
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+
+        return result;
+    }
+
+    // 이건 그냥 귀찮음
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    // 어려움
+    // Order를 기준으로 본다면
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    // 어렵다
+    // 이거는 한방에 데이터(컬렉션때매 뻥튀기상태) 땡겨와서 로직으로 지지고 볶아서 내가 원하는 형태로 DTO에 담는거임.
+    // 이렇게 하는거 아니면 ToOne 먼저 한번에 join sql날려서 DTO에 담고, 그다음 for문으로 컬렉션 1+N 쿼리날리면서 DTO에 저장하거나 , 자료구조 쓰거나 하면서 해야함.
+    // 페이징 불가능 -> ToMany 조인때문에 그럼.
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+    }
 
 
     @Data
